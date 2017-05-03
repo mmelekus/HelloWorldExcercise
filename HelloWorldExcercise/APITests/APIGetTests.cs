ea72ac09d1d974f395ce594f29800e1d7d9668fb
Martin Melekus <mmelekus@hotmail.com>
2017-05-02 19:53:14 -0500

    Completed unit tests for DAL and API

---
+++ HelloWorldExcercise/APITests/APIGetTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileAccess;
+using Microsoft.Extensions.Options;
+using Moq;
+using HelloWorldAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using HelloWorldModels;
+
+namespace DALTests
+{
+    [TestClass]
+    public class APIGetTests
+    {
+        private readonly HelloWorldFile _helloWorldFile;
+        private readonly WorldMessageController _messageController;
+
+        public APIGetTests()
+        {
+            var mock = new Mock<IOptions<FileOptions>>();
+            mock.Setup(option => option.Value).Returns(() => new FileOptions() { FileName = @"Data\HelloWorldMessages.csv" });
+            _helloWorldFile = new HelloWorldFile(mock.Object);
+            _messageController = new WorldMessageController(_helloWorldFile);
+        }
+
+        [TestMethod]
+        public void GetReturnsNoMessage()
+        {
+            var message = _messageController.Get("3") as OkObjectResult;
+
+            Assert.IsNotNull(message, "Message is null");
+            Assert.IsTrue(message.StatusCode == 200, "StatusCode is not 200");
+            Assert.IsNull(message.Value, "Message value is not null");
+        }
+
+        [TestMethod]
+        public void GetFirstMessage()
+        {
+            var message = _messageController.Get() as OkObjectResult;
+
+            Assert.IsNotNull(message, "Message is null");
+            Assert.IsTrue(message.StatusCode == 200, "StatusCode is not 200");
+            Assert.AreEqual("Hello World!", ((HelloWorldMessage)message.Value).Message, true, "Message count is not greater than 0");
+        }
+
+        [TestMethod]
+        public void GetFirstMessageById()
+        {
+            var message = _messageController.Get("1") as OkObjectResult;
+
+            Assert.IsNotNull(message, "Message is null");
+            Assert.IsTrue(message.StatusCode == 200, "StatusCode is not 200");
+            Assert.AreEqual("Hello World!", ((HelloWorldMessage)message.Value).Message, true, "Message count is not greater than 0");
+        }
+    }
+}
---
+++ HelloWorldExcercise/DALTests/FileAccessTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileAccess;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace DALTests
+{
+    [TestClass]
+    public class FileAccessTests
+    {
+        private readonly HelloWorldFile _helloWorldFile;
+
+        public FileAccessTests()
+        {
+            var mock = new Mock<IOptions<FileOptions>>();
+            mock.Setup(option => option.Value).Returns(() => new FileOptions() { FileName = @"Data\HelloWorldMessages.csv" });
+            _helloWorldFile = new HelloWorldFile(mock.Object);
+        }
+
+        [TestMethod]
+        public void ReturnNoMessage()
+        {
+            var result = _helloWorldFile.GetItem("3");
+            Assert.IsNull(result.FirstOrDefault(), "Result is not null");
+        }
+
+        [TestMethod]
+        public void ReturnAllMessages()
+        {
+            var result = _helloWorldFile.GetAll().ToList();
+            Assert.IsTrue(result.Count > 0, "Count is not greater than zero");
+        }
+
+        [TestMethod]
+        public void ReturnFirstMessage()
+        {
+            var testMessage = "Hello World!";
+            var result = _helloWorldFile.GetItem("1").FirstOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testMessage, result.Message, $"First message is not {testMessage}, but is {result.Message}");
+        }
+    }
+}
